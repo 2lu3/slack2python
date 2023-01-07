fa63ff4f5c13d4ebe8cbaaff1127ae1d9d3a59b4
2lu3 <hi2lu3@gmail.com>
2023-01-07 17:44:17 +0900

    appをユーザー側で初期化するようにした

--- slack2python/client.py
+++ slack2python/client.py
@@ -5,9 +5,15 @@ from slack_sdk import WebClient
 _app: Optional[App] = None
 _client: Optional[WebClient] = None
 
+def set_app(app: App):
+    """This function must be called before 'app.start()'"""
+    global _app, _client
+    _app = app
+    _client = _app.client
+
 
 def raise_uninitialized_error():
-    raise RuntimeError("Please use init() to initialize slack2python")
+    raise RuntimeError("Please use set_app() to initialize slack2python")
 
 
 def app() -> App:
@@ -20,39 +26,3 @@ def client() -> WebClient:
     if _client is None:
         raise_uninitialized_error()
     return _client
-
-
-def init(bot_user_oauth_token: str, signing_secret: str):
-    """initialize slack2python
-
-    To get the arguments of this function, follow the steps.
-    1. Go to https://api.slack.com/apps/
-    2. Click your app
-    3. Copy 'Singing Secret'
-    4. Click 'OAuth & Permissions'
-    5. Add at least 1 bot token scopes if no scope registered
-    6. Copy 'Bot User OAuth Token'
-
-    Args:
-        bot_user_oauth_token (str): Bot User OAuth Token
-        signing_secret (str): Singing Secret
-    """
-    global _app, _client
-
-    if _app is not None or _client is not None:
-        raise RuntimeError("init() cannot be executed twice or more")
-
-    _app = App(
-        token=bot_user_oauth_token,
-        signing_secret=signing_secret,
-    )
-    _client = _app.client
-
-
-def start(port: int = 8000):
-    """start a slack bolt session
-
-    Args:
-        port (int): port to listen slack events
-    """
-    app().start(port=port)
